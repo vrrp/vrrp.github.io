f4e20d935a83e4c73dd8bb1e1eea27ac74eec97d
vrrp <vr.rojaspozo@gmail.com>
2024-08-28 21:58:25 -0500

    21:58:25 28-August-2024

--- script.js
+++ script.js
@@ -1,65 +1,46 @@
-const categories = {
-    category1: [
-        'images/category1/img1.png',
-        'images/category1/img2.png',
-        'images/category1/img3.png'
-    ],
-    category2: [
-        'images/category2/img1.png',
-        'images/category2/img2.png',
-        'images/category2/img3.png'
-    ],
-    category3: [
-        'images/category3/img1.png',
-        'images/category3/img2.png',
-        'images/category3/img3.png'
-    ]
-};
+// Array of image paths
+const images = [
+    'images/image1.png',
+    'images/image2.png',
+    'images/image3.png'
+];
 
-let currentCategory = 'category1';
 let currentIndex = 0;
 let intervalId = null;
 
+// Function to update the displayed image
 function updateImage() {
-    document.getElementById('slider-image').src = categories[currentCategory][currentIndex];
+    document.getElementById('slider-image').src = images[currentIndex];
 }
 
+// Function to go to the next image
 function nextImage() {
-    currentIndex = (currentIndex + 1) % categories[currentCategory].length;
+    currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 }
 
+// Function to go to the previous image
 function prevImage() {
-    currentIndex = (currentIndex - 1 + categories[currentCategory].length) % categories[currentCategory].length;
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 }
 
+// Function to start the slideshow
 function playSlideshow() {
     if (!intervalId) {
-        intervalId = setInterval(nextImage, 500);
+        intervalId = setInterval(nextImage, 500);// intervals in milisecons (3000)
     }
 }
 
+// Function to stop the slideshow
 function stopSlideshow() {
     clearInterval(intervalId);
     intervalId = null;
 }
 
-function selectCategory(category) {
-    currentCategory = category;
-    currentIndex = 0;
-    updateImage();
-    stopSlideshow();
-}
-
+// Attach event listeners to the buttons
 document.getElementById('nextButton').addEventListener('click', nextImage);
 document.getElementById('prevButton').addEventListener('click', prevImage);
 document.getElementById('playButton').addEventListener('click', playSlideshow);
 document.getElementById('stopButton').addEventListener('click', stopSlideshow);
-
-document.querySelectorAll('.category-selector button').forEach(button => {
-    button.addEventListener('click', () => {
-        selectCategory(button.getAttribute('data-category'));
-    });
-});
 
