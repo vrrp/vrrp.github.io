a6da9c7310cd5bb225c32e2a4e114ad53a9279d2
vrrp <vr.rojaspozo@gmail.com>
2024-08-28 21:45:24 -0500

    21:45:24 28-August-2024

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
+# app.py
 from flask import Flask, render_template
+import pandas as pd
+import plotly.express as px
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # List of image filenames to be animated
-    images = [
-        "iheight_of_zero_isotherm_vilcanota_valley_day_jan2019.png",
-        "iheight_of_zero_isotherm_vilcanota_valley_day_feb2019.png",
-        "iheight_of_zero_isotherm_vilcanota_valley_day_mar2019.png"
-    ]
-    return render_template('index.html', images=images)
+def dashboard():
+    # Load your data
+    df = pd.read_csv('data.csv')
+
+    # Create a time series line chart
+    fig1 = px.line(df, x='Date', y='Value1', title='Time Series Curve')
+    graph1 = fig1.to_html(full_html=False)
+
+    # Create a bar chart
+    fig2 = px.bar(df, x='Date', y='Value2', title='Time Series Bar')
+    graph2 = fig2.to_html(full_html=False)
+
+    return render_template('index.html', graph1=graph1, graph2=graph2)
 
 if __name__ == '__main__':
     app.run(debug=True)
