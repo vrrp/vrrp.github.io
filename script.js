c0c6ad0a6c1954cdaa4a639319d3556a86e2c628
vrrp <vr.rojaspozo@gmail.com>
2024-08-28 21:50:41 -0500

    21:50:41 28-August-2024

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-// Array of image paths
-const images = [
-    'images/image1.png',
-    'images/image2.png',
-    'images/image3.png'
-];
+const categories = {
+    category1: [
+        'images/category1/img1.png',
+        'images/category1/img2.png',
+        'images/category1/img3.png'
+    ],
+    category2: [
+        'images/category2/img1.png',
+        'images/category2/img2.png',
+        'images/category2/img3.png'
+    ],
+    category3: [
+        'images/category3/img1.png',
+        'images/category3/img2.png',
+        'images/category3/img3.png'
+    ]
+};
 
+let currentCategory = 'category1';
 let currentIndex = 0;
+let intervalId = null;
 
-// Function to change the image
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById('slider-image').src = images[currentIndex];
+function updateImage() {
+    document.getElementById('slider-image').src = categories[currentCategory][currentIndex];
 }
 
-// Set an interval to change the image every 3 seconds
-setInterval(changeImage, 1000);
+function nextImage() {
+    currentIndex = (currentIndex + 1) % categories[currentCategory].length;
+    updateImage();
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + categories[currentCategory].length) % categories[currentCategory].length;
+    updateImage();
+}
+
+function playSlideshow() {
+    if (!intervalId) {
+        intervalId = setInterval(nextImage, 500);
+    }
+}
+
+function stopSlideshow() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+function selectCategory(category) {
+    currentCategory = category;
+    currentIndex = 0;
+    updateImage();
+    stopSlideshow();
+}
+
+document.getElementById('nextButton').addEventListener('click', nextImage);
+document.getElementById('prevButton').addEventListener('click', prevImage);
+document.getElementById('playButton').addEventListener('click', playSlideshow);
+document.getElementById('stopButton').addEventListener('click', stopSlideshow);
+
+document.querySelectorAll('.category-selector button').forEach(button => {
+    button.addEventListener('click', () => {
+        selectCategory(button.getAttribute('data-category'));
+    });
+});
 
