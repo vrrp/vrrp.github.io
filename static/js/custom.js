01db05b1787c1602847e19edbff90f30268d9707
vrrp <vr.rojaspozo@gmail.com>
2024-08-31 01:21:20 -0500

    01:21:20 31-August-2024

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,71 +1,46 @@
-// Write your Javascript here
-//-----------------------------------------------------------------------------------------
+// Array of image paths
+const images = [
+    'images/image1.png',
+    'images/image2.png',
+    'images/image3.png'
+];
 
-// image slides
-//-----------------------------------------------------------------------------------------
-const images = {
-    categoria1: [
-        '{{ "/images/categoria1/img1.png" | relative_url }}',
-        '{{ "/images/categoria1/img2.png" | relative_url }}',
-        '{{ "/images/categoria1/img3.png" | relative_url }}'
-    ],
-    categoria2: [
-        '{{ "/images/categoria2/img1.png" | relative_url }}',
-        '{{ "/images/categoria2/img2.png" | relative_url }}',
-        '{{ "/images/categoria2/img3.png" | relative_url }}'
-    ],
-    categoria3: [
-        '{{ "/images/categoria3/img1.png" | relative_url }}',
-        '{{ "/images/categoria3/img2.png" | relative_url }}',
-        '{{ "/images/categoria3/img3.png" | relative_url }}'
-    ]
-};
-
-let currentCategory = 'categoria1';
 let currentIndex = 0;
 let intervalId = null;
 
+// Function to update the displayed image
 function updateImage() {
-    document.getElementById('slider-image').src = images[currentCategory][currentIndex];
+    document.getElementById('slider-image').src = images[currentIndex];
 }
 
+// Function to go to the next image
 function nextImage() {
-    currentIndex = (currentIndex + 1) % images[currentCategory].length;
+    currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 }
 
+// Function to go to the previous image
 function prevImage() {
-    currentIndex = (currentIndex - 1 + images[currentCategory].length) % images[currentCategory].length;
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 }
 
+// Function to start the slideshow
 function playSlideshow() {
     if (!intervalId) {
-        intervalId = setInterval(nextImage, 500);
+        intervalId = setInterval(nextImage, 500);// intervals in milisecons (3000)
     }
 }
 
+// Function to stop the slideshow
 function stopSlideshow() {
     clearInterval(intervalId);
     intervalId = null;
 }
 
-function changeCategory(category) {
-    currentCategory = category;
-    currentIndex = 0;
-    updateImage();
-    stopSlideshow();
-}
-
+// Attach event listeners to the buttons
 document.getElementById('nextButton').addEventListener('click', nextImage);
 document.getElementById('prevButton').addEventListener('click', prevImage);
 document.getElementById('playButton').addEventListener('click', playSlideshow);
 document.getElementById('stopButton').addEventListener('click', stopSlideshow);
-
-document.querySelectorAll('.category-button').forEach(button => {
-    button.addEventListener('click', () => {
-        changeCategory(button.dataset.category);
-    });
-});
-
 
