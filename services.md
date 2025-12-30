2dfb25a416f693110d8181b50c219ed030d9e925
vrrp <vr.rojaspozo@gmail.com>
2025-12-29 23:53:52 -0500

    up all

--- services.md
+++ services.md
@@ -2,75 +2,34 @@
 title: Services
 layout: page
 ---
-<H1 align="left"><span style="font-family:Times New Roman;font-size:150%;color:darkmagenta"><b>Services</b></span> </H1>
-
-<h2>Agriculture</h2>
-<ul class="Agriculture">
-	<li>Climatic studies of thermal suitability indices to identify areas and periods of time, where temperature conditions are optimal for crop development.</li>	
-</ul>
-
-<h2>Atmosphere</h2>
-<ul class="Atmosphere">
-	<li>Climatic studies of precipitation, air temperature, air quality, cloud cover and wind speed.</li>	
-</ul>
-
-<h2>Oceanography</h2>
-<ul class="Oceanography">
-	<li>Climatic studies of sea surface temperature, sea surface salinity, ocean surface currents and biogeochemical variables.</li>
-</ul>  
-
-<h2>Hydrology and glaciology</h2>
-<ul class="Hydrology">
-	<li>Climatic studies of river flow</li>
-	<li>Climatic studies of snow depth and surface fluxes from snow melt (just for tropical glaciers).</li>
-</ul>
-
-<h2>Monitoring System</h2>
-<!--p> La gestión de recursos hídricos es el proceso de planificar, desarrollar, distribuir y gestionar los recursos de agua
-de manera eficiente y sostenible para satisfacer las necesidades de la sociedad, la economía y el medio ambiente
-
-Objetivos
-    1. Garantizar la disponibilidad de agua para el consumo humano, agricultura, industria y ecologia
-    2. Proteger la calidad del agua y prevenir la contaminacion
-    3. Promover el uso eficiente del agua y reducir sus péridad
-    4. Mitigar los efectos del cambio climático y los desastres naturales
-</p -->
-<h4>Tracking the ocean part of El Niño-Southern Oscillation (ENSO) climate pattern.</h4>
-<p style="text-align: left;">According to <a href="https://www.climate.gov/news-features/understanding-climate/climate-variability-oceanic-nino-index" style="color:darkmagenta">NOAA</a>, the blue, yellow, and red polygons correspond to the ocean temperature monitoring in the El Niño 1+2, 3, and 3+4 regions, respectively.</p>
+<H1 align="left"><span style="font-family:Times New Roman;font-size:150%;color:darkmagenta"><b>Monitoreo Energía Electrica</b></span> </H1>
 
 <div class="graficas-container">
 	<div id="map"></div>
+	
+	
 	<div id="charts">
             <div id="time-range">
-                <h3>Period of time</h3>
+                <!--h3>Rango de tiempo:</h3-->
+                
                 <div class="slider-container">
-                    <label>Start date: <span id="start-date">1982-01</span></label>
-                    <input type="range" id="start-slider" min="0" max="522" value="0"> <!-- max="365"-->
+                    <label>Inicio: <span id="start-date">1985</span></label>
+                    <input type="range" id="start-slider" min="0" max="40" value="0"> 
                 </div>
                 <div class="slider-container">
-                    <label>End date  : <span id="end-date">2025-07</span></label>
-                    <input type="range" id="end-slider" min="0" max="522" value="522">
+                    <label>Fin: <span id="end-date">2024</span></label>
+                    <input type="range" id="end-slider" min="0" max="40" value="40">
                 </div>
             </div>
+            
             <div class="chart-container">
                 <!--h3>Gráfico de Barras</h3-->
                 <canvas id="barChart" data-img="{{ '/assets/images/tu_imagen.png' | relative_url }}"></canvas>
             </div>
-        </div>
-	<!--div align="center" id="tabla_ensoONI"></div-->
-	<!--div align="center" id="plot_timeSeries_ensoIndex"></div-->
-	<p> Data taken from <a href="https://www.cpc.ncep.noaa.gov/data/indices/" style="color:darkmagenta"> Climate Prediction Center - NOAA</a>, and dynamic visualization development by <a href="https://vrrp.github.io/" style="color:darkmagenta">Sacha Analytics</a>.</p>
-	
+    </div>
 </div>
-<!--script src="/static/js/script_mapaENSOv1.js"></script-->
-<!--script src="/static/js/script_mapaENSOv2.js"></script-->
-<script src="/static/js/script_mapaENSOv2.js"></script>
 
-
-<h4 align="left">Public health monitoring: Anemia case study (We are working asap!).</h4>
-<!--script src="/static/js/script_tablaENSO.js"></script-->
-<!--script src="/static/js/plot_timeSeries_ensoIndex.js"></script-->
-<p style="text-align: left;">To visualized our products in <a href="https://vrrp.github.io/login/" style="color:darkmagenta">Monitoring</a>, email us to suscribe.</p>
+<script src="/static/js/script_mapa_energia.js"></script>
 
 
 
---
+++ static/js/script_mapa_energia.js
@@ -0,0 +1,193 @@
+// URL del archivo CSV (cámbiala por la tuya)
+ //------------------------------------------------------------------------
+ const csvUrl = 'https://raw.githubusercontent.com/vrrp/vrrp.github.io/refs/heads/main/assets/data/energia_electrica.csv'; // Reemplaza con una URL válida
+ // Usar fetch para obtener el CSV
+Papa.parse(csvUrl, {
+    download: true,
+    header: true,
+    complete: function(results) {
+        csvData = results.data;
+        console.log("Datos importados:", csvData);
+        // Aquí puedes usar csvData en tu aplicación
+         // crear estructuras de datos
+         let dates=[], peru=[], colombia=[], brasil=[];   
+         csvData.forEach(irow =>{
+             dates.push(irow['Year']);
+             peru.push(parseFloat(irow['Peru_Generation_kwh']));
+             colombia.push(parseFloat(irow['Colombia_Generation_kwh']));
+             brasil.push(parseFloat(irow['Brazil_Generation_kwh']));
+             });
+        let n_elem = dates.length-2;
+        //console.log(n_elem);
+        //console.log(dates[0]);
+        //console.log(dates[n_elem]);
+        
+        // extraer valores min/mx de la data para extablecer inf y sup del eje y
+        const minData = Math.min(...peru);
+        const maxData = Math.max(...peru);
+        //console.log(minData[0]);
+
+        
+        // Inicializar el mapa
+        const map = L.map('map').setView([-5, -120], 2);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+           // attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+        const locations = [
+            { varname: 'ONI (Nino 1+2)', nameRegion:"Peru - Total per capita (kwh)", coords: [-12.042148, -77.043740], color: '#FF5733', data: peru },
+            { varname: 'ONI (Nino 3)', nameRegion:"Brasil - Total per capita (kwh)", coords: [-14.042148, -46.043740], color: '#f1ee23', data: brasil },
+            { varname: 'ONI (Nino 3+4)', nameRegion:"Colombia - Total per capita (kwh)", coords: [4.042148, -74.343740], color: '#2912CC', data: colombia }
+        ];
+        
+        let selectedLocation = locations[0];
+        const markers = locations.map(loc => {
+            return L.circleMarker(loc.coords,{
+            	radius:8,
+                fillColor: loc.color,
+                color: loc.color,
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.5
+                }).addTo(map)
+                .bindPopup(loc.nameRegion)
+                .on('click', () => {
+                    selectedLocation = loc;
+                    updateCharts();
+                });
+        });
+
+        // Configurar fechas
+        //--------------------------------------------------------------
+        // generar fechas diarias
+        const startDate = new Date(dates[0]);
+        const getDateFromDay = (day) => {
+            const date = new Date(startDate);
+            date.setDate(startDate.getDate() + day);
+            return date.toISOString().split('T')[0];
+        };
+
+        // generar fechas mensuales
+        const getDateFromMonth = (month) => {
+            const date = new Date(startDate);
+            date.setMonth(startDate.getMonth() + month);
+            return date.toISOString().slice(0, 7);
+          };
+          
+         // generar fechas anuales
+         const startYear = dates[0];
+         const endYear = dates[n_elem];
+         const annualDates = [];
+         
+         for (let year = startYear; year <= endYear; year++) {
+             annualDates.push(year.toString());
+             }
+         console.log(annualDates);
+
+        // Sliders
+        //--------------------------------------------------------------
+        const startSlider = document.getElementById('start-slider');
+        const endSlider = document.getElementById('end-slider');
+        
+        const startDateLabel = document.getElementById('start-date');
+        const endDateLabel = document.getElementById('end-date');
+
+        startSlider.addEventListener('input', () => {
+            if (parseInt(startSlider.value) > parseInt(endSlider.value)) {
+                startSlider.value = endSlider.value;
+            }
+            //startDateLabel.textContent = getDateFromDay(parseInt(startSlider.value));
+            //startDateLabel.textContent = getDateFromMonth(parseInt(startSlider.value));
+            startDateLabel.textContent = annualDates;
+            updateCharts();
+        });
+
+        endSlider.addEventListener('input', () => {
+            if (parseInt(endSlider.value) < parseInt(startSlider.value)) {
+                endSlider.value = startSlider.value;
+            }
+            //endDateLabel.textContent = getDateFromDay(parseInt(endSlider.value));
+            //endDateLabel.textContent = getDateFromMonth(parseInt(endSlider.value));
+            endDateLabel.textContent = annualDates;
+            updateCharts();
+        });
+        console.log(startSlider);
+
+        // Configurar gráficos
+        //--------------------------------------------------------------
+        //const lineCtx = document.getElementById('lineChart').getContext('2d');
+        const barCtx = document.getElementById('barChart').getContext('2d');
+        /*
+        const lineChart = new Chart(lineCtx, {
+            type: 'line',
+            data: { labels: [], datasets: [{ label: 'Datos', data: [], borderColor: 'blue', fill: false }] },
+            options: { responsive: true, scales: { y: { beginAtZero: true } } }
+        });*/
+
+        const barChart = new Chart(barCtx, {
+            type: 'bar',
+            data: { labels: [], 
+                    datasets: [{ 
+                                label: 'Datos',
+                                data: [], 
+                                //backgroundColor: 'rgba(75, 192, 192, 0.2)', 
+                                backgroundColor: function(context) {
+                                    var index = context.dataIndex;
+                                    var value = context.dataset.data[index];
+                                    return value < 0 ? 'blue' : 'red';
+                                    },
+                                //borderColor: 'rgba(75, 192, 192, 1)', 
+                                borderColor: function(context) {
+                                    var index = context.dataIndex;
+                                    var value = context.dataset.data[index];
+                                    return value < 0 ? 'black' : 'blue';
+                                    },
+                                borderWidth: 1 
+                            }]
+                  },
+            options: { responsive: true, 
+                       //scales: { y: { beginAtZero: true } } 
+                       scales: {
+                        y: {
+                        min: 0, // Límite inferior del eje Y
+                        max: 3600   // Límite superior del eje Y
+                        }}
+                     },
+            //plugins: [imageBackgroundPlugin] 
+        });
+
+          // Actualizar gráficos
+        //--------------------------------------------------------------
+        function updateCharts() {
+            const startDay = parseInt(startSlider.value);
+            const endDay = parseInt(endSlider.value);
+            
+            const labels = [];
+            
+            for (let i = 0; i <= n_elem; i++) {
+               labels.push(annualDates[i]);
+            }
+
+            
+            const data = selectedLocation.data.slice(startDay, endDay + 1);
+            console.log(labels);
+            /*
+            lineChart.data.labels = labels;
+            lineChart.data.datasets[0].data = data;
+            lineChart.data.datasets[0].label = selectedLocation.varname;
+            lineChart.update();*/
+
+            barChart.data.labels = labels;
+            barChart.data.datasets[0].data = data;
+            barChart.data.datasets[0].label = selectedLocation.nameRegion;
+            barChart.update();
+        }
+
+        // Inicializar gráficos
+        //--------------------------------------------------------------
+        updateCharts();
+    },
+    error: function(error) {
+        console.error("Error al importar el CSV:", error);
+    }
+    });
